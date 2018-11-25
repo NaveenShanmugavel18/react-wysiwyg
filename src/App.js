956995531c5889cf2765359d1e9ded6f37d35945
Naveen Shanmugavel <naveensv18@gmail.com>
2018-11-26 00:48:53 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'semantic-ui-react';
+import { EditorState, convertToRaw } from 'draft-js';
+import { Editor } from 'react-draft-wysiwyg';
+import draftToHtml from 'draftjs-to-html';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
+  
+  state = { editorState: EditorState.createEmpty()  }
+  
+  onChange = (editorState) => this.setState({editorState});
+  
+  handleSubmit = (e) => {
+  	e.preventDefault();
+  	var convertedData = draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+  	var theDiv = document.getElementById("appended-html-content");
+		var content = document.createTextNode(convertedData);
+		theDiv.appendChild(content);
+  	this.setState({editorState: EditorState.createEmpty()})
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Editor 
+					editorState={this.state.editorState} 
+					wrapperClassName="demo-wrapper" 
+					editorClassName="editer-content"
+					onEditorStateChange={this.onChange}
+				/>
+				<div id="comment-button-div">
+        	<Button onClick={this.handleSubmit} id="comment-submit-button" color="teal">Submit</Button>
+      	</div>
+      	<div id="appended-html-content">
+      	</div>
       </div>
+      
     );
   }
 }
